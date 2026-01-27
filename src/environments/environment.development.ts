df423e2646bcdf9fa5c8ddc650aae9e47f33347d
Michael Ball <aaronhunter@live.com>
2026-01-26 22:57:22 -0600

    update hostUrl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     landingPageUrl: 'http://localhost:4200',
-    hostUrl: 'http://localhost:4204/pokedexapi'
+    hostUrl: 'http://localhost:4204/combined'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     landingPageUrl: 'https://mypokedex.us',
-    hostUrl: 'https://mypokedex.us/pokedexapi'
+    hostUrl: 'https://mypokedex.us/springboot'
 };
