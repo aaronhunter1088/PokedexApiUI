fccb5b3892736bd2d1686d6573fdca19c5882ea3
Michael Ball <aaronhunter@live.com>
2026-02-22 11:29:25 -0600

    fix mobile pokemon-list not rendering. minor cleanup

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,5 +5,11 @@ export const environment = {
         const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
         return isMobile ? 'http://192.168.1.152:4200' : 'http://localhost:4200';
     },
-    hostUrl: 'http://localhost:4204/combined'
+    get hostUrl(): string {
+        const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        const host = window.location.hostname;
+        const proto = window.location.protocol; // "http:" or "https:"
+        const apiPort = proto === 'http:' ? ':4204' : ''; // same here, but you can customize
+        return `${proto}//${host}${apiPort}/combined`;
+    }
 };
